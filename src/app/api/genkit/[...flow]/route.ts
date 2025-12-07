917966dc063efa73e8a5baa7d3bf18f44fcd52e2
Pedro Rafael <pedrorafael7337@gmail.com>
2025-12-07 05:25:29 +0000

    Try fixing this error: `Build Error: Export nextHandler doesn't exist in

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { editCode } from '@/ai/flows/edit-code-flow';
-import { nextHandler } from '@genkit-ai/next';
+import '@/ai/flows/edit-code-flow';
+import {nextHandler} from '@genkit-ai/next';
 
 export const POST = nextHandler();
