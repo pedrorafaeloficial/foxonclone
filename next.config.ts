5d71e1fa66871e025a3e2ee3de496cd26179bed9
Pedro Rafael <pedrorafael7337@gmail.com>
2025-12-07 04:59:33 +0000

    coloca o logo da FoxOn na Ferramenta: https://agenciafoxon.com.br/wp-con

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'agenciafoxon.com.br',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,22 +1,14 @@
+import Image from 'next/image';
 import type { SVGProps } from "react";
 
 export const Icons = {
   logo: (props: SVGProps<SVGSVGElement>) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M4 12h16" />
-      <path d="m8 8-4 4 4 4" />
-      <path d="M14 4h2a2 2 0 0 1 2 2v2" />
-      <path d="M4 14v2a2 2 0 0 0 2 2h2" />
-      <path d="M20 10V8a2 2 0 0 0-2-2h-2" />
-    </svg>
+    <Image
+      src="https://agenciafoxon.com.br/wp-content/uploads/2023/11/cropped-Ativo-1-1.png"
+      alt="FoxOn Logo"
+      width={32}
+      height={32}
+      className={props.className}
+    />
   ),
 };
